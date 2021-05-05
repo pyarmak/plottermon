1ca3c6f24f7c7d8734a9aeb8ab2c494a4a6f3a4f
Pavel Yarmak <pyarmak@gmail.com>
2021-05-05 12:41:24 -0500

    Added cpu and time stats to print command

--- logAnalyzer.js
+++ logAnalyzer.js
@@ -17,6 +17,8 @@ class LogAnalyzer {
             { name: "Done", steps: 1, cumprog: 22, pattern: "" },
         ];
 
+        this.stats = {};
+
         this.SPINNER = "◴◷◶◵";
 
         this.TOTAL_PROGRESS = 22;
@@ -72,11 +74,70 @@ class LogAnalyzer {
         return [phase, stage, done];
     }
 
+    extractPhaseStats(name, line) {
+        line = line.trimStart();
+        // Use a basic string operation first for performance optimization
+        // String.startsWith is 2x faster than matching our regex pattern
+        if (!line.startsWith('Time for phase')) return;
+        const pattern = /Time for (?<phase>phase \d) = (?<time>.*?) seconds. CPU \((?<cpu>.*?)%\)/;
+        const match = line.match(pattern);
+        if (!match) return;
+        const { phase, time, cpu } = match.groups;
+        if (!this.stats[name]) this.stats[name] = {};
+        if (!this.stats[name][phase]) this.stats[name][phase] = [];
+        this.stats[name][phase].push({
+            time: parseFloat(time),
+            cpu: parseFloat(cpu)
+        });
+    }
+
+    getPlotDescriptiveStats(stats) {
+        const cpuData = stats.map(v => v.cpu);
+        const timeData = stats.map(v => v.time);
+        const cpuStats = this.getMeanAndStandardDeviation(cpuData);
+        const timeStats = this.getMeanAndStandardDeviation(timeData);
+        return {
+            Mean: {
+                'CPU (%)': this.round(cpuStats[0]),
+                'Time (s)': this.round(timeStats[0])
+            },
+            SD: {
+                'CPU (%)': this.round(cpuStats[1]),
+                'Time (s)': this.round(timeStats[1])
+            }
+        }
+    }
+
+    round(num) {
+        return +(Math.round(num + "e+3") + "e-3");
+    }
+
+    getMeanAndStandardDeviation(array) {
+        const n = array.length
+        const mean = array.reduce((a, b) => a + b) / n
+        const sd = Math.sqrt(array.map(x => Math.pow(x - mean, 2)).reduce((a, b) => a + b) / n);
+        return [mean, sd];
+    }
+
     async processFile(filePath, name) {
         const fileData = fs.createReadStream(filePath, { encoding: 'utf8' });
-        const initialProgress = await this.getInitialProgress(fileData.pipe(split2()));
-        if (this.command == 'print')
-            console.log(`[${name}] Done ${initialProgress.done}. Current plot in phase ${initialProgress.phase} stage ${initialProgress.stage}/${this.PHASES[initialProgress.phase].steps}`);
+        const initialProgress = await this.getInitialProgress(fileData.pipe(split2()), name);
+        if (this.command == 'print') {
+            console.log(`[${name}] Completed ${initialProgress.done} plots.`);
+            console.log(`Current plot in phase ${initialProgress.phase} stage ${initialProgress.stage}/${this.PHASES[initialProgress.phase].steps}`);
+            for (const [phase, stats] of Object.entries(this.stats[name])) {
+                console.log(`${phase} stats:`);
+                let tableData = {};
+                stats.forEach((v, i) => {
+                    tableData[`Plots #${i+1}`] = {
+                        'CPU (%)': v.cpu,
+                        'Time (s)': v.time
+                    }
+                });
+                tableData = Object.assign(tableData, this.getPlotDescriptiveStats(stats));
+                console.table(tableData);
+            }
+        }
         else if (this.command == 'watch') {
             this.watchFileProgress(name, filePath, initialProgress);
         }
@@ -86,7 +147,6 @@ class LogAnalyzer {
         let currentProgress = initialProgress;
         currentProgress.spinner = 0;
 
-        // this.ui.setProgress(name, this.calculateProgress(initialProgress), this.getTitle(name, currentProgress));
         process.send({
             type: messages.PROGRESS_UPDATE,
             payload: [
@@ -107,11 +167,11 @@ class LogAnalyzer {
             .on('data', (line) => {
                 const curPhase = currentProgress.phase;
                 const res = this.parseLine(line, curPhase);
+                this.extractPhaseStats(name, line);
                 if (res[0] !== undefined) currentProgress.phase = res[0];
                 if (res[1] !== undefined) currentProgress.stage = res[1];
                 if (res[2]) currentProgress.done++;
                 (currentProgress.spinner === this.SPINNER.length - 1) ? currentProgress.spinner = 0 : currentProgress.spinner++;
-                // this.ui.setProgress(name, this.calculateProgress(currentProgress), this.getTitle(name, currentProgress));
                 process.send({
                     type: messages.PROGRESS_UPDATE,
                     payload: [
@@ -134,12 +194,13 @@ class LogAnalyzer {
         return Math.round((res / this.TOTAL_PROGRESS) * 100);
     }
 
-    async getInitialProgress(file) {
+    async getInitialProgress(file, name) {
         let phase, stage;
         let done = 0;
         for await (let line of file) {
             if (line.startsWith("\t")) continue;
             const res = this.parseLine(line, phase);
+            this.extractPhaseStats(name, line);
             if (res[0] !== undefined) phase = res[0];
             if (res[1] !== undefined) stage = res[1];
             if (res[2]) done++;
